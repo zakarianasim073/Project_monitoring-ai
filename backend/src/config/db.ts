6f6480c3d1c6be1bb2edbe02fcd37868c79d911c
zakarianasim073 <128282115+zakarianasim073@users.noreply.github.com>
2026-03-27 02:20:05 +0600

    Update db.ts

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,1 +1,14 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log('✅ MongoDB Connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
